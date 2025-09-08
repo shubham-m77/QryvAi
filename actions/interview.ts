8de6d23fc309b583d5f24f3eef9af19441a223f0
Shubham Mandal <shubham.m9022@gmail.com>
2025-09-08 13:02:17 +0530

    Fixed Errors

--- actions/interview.ts
+++ actions/interview.ts
@@ -14,6 +14,6 @@ export async function generateQuiz() {
         where: { id: userId }
     })
     if (!user) throw new Error("User not found!")
-    const prompt = `Generate 10 technical interview questions for a ${user.skills.}`
+    // const prompt = `Generate 10 technical interview questions for a ${user.skills.}`
 
 }
